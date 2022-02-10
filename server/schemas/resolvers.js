a94bb2b1b72a6a81305c71c6e98618595b9ba3f0
NIGHT LIGHT <jaredinyaagha@gmail.com>
2022-02-09 19:52:23 -0500

    added create user resolver function

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,6 +14,11 @@ const resolvers = {
     },
 
     Mutation: {
+        createUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
             if (!user) {
